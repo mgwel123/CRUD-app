ffa2212517bfeb1ddf64a64143500c93db824531
Michael Welker <mikegw224@gmail.com>
2022-04-23 18:29:02 -0700

    POST wrapper function

--- index.js
+++ index.js
@@ -9,67 +9,70 @@ let output = '';
 const url = 'https://crudcrud.com/api/8b25b7bf8acd48c694935f90eb63c6b8/icecream';
 
 
-createIceCream.addEventListener('click', () => {
+createIceCream.addEventListener('click', postNewFlavor);
 
-});
-
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "flavor": "flavor",
-        "scoops": Number, 
+function postNewFlavor(){
+    
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "flavor": "flavor",
+            "scoops": Number, 
+        })
     })
-})
-    .then(response => response.json())
-
-    .then(data => {
-        data.forEach(post => {
-            output += `
-            <div class="card">
-            <div class="card-body">
-                <h2 class="card-title">${post.title}</h2>
-                <p class="card-title"></p>
-                <br>
-                <div class="row">
-                    <div class="col-sm">
-                        <div class="form-group">
-                            <label for="toppings" class="form-label">Add Toppings:</label>
-                            <input id="toppings" type="text" class="form-control" placeholder="sprinkles, syrup, candy etc.">
-                            <br>
+        .then(response => response.json())
+    
+        .then(data => {
+            data.forEach(post => {
+                output += `
+                <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">${post.title}</h2>
+                    <p class="card-title"></p>
+                    <br>
+                    <div class="row">
+                        <div class="col-sm">
+                            <div class="form-group">
+                                <label for="toppings" class="form-label">Add Toppings:</label>
+                                <input id="toppings" type="text" class="form-control" placeholder="sprinkles, syrup, candy etc.">
+                                <br>
+                                
+                            </div>
+    
+                        </div>
+                        <div class="col-sm">
+                            <div class="form-group">
+                                <button id="addToppings" type="button" class="btn btn-primary btn-sm">Add Topping</button>
+                            </div>
                             
                         </div>
-
+    
                     </div>
-                    <div class="col-sm">
-                        <div class="form-group">
-                            <button id="addToppings" type="button" class="btn btn-primary btn-sm">Add Topping</button>
+    
+                    <div class="row align-items-center">
+                        <div class="col-sm">
+                            <p>new toppings here</p>
+    
                         </div>
-                        
+                        <div class="col-sm">
+                            <button id="deleteTopping" type="button" class="btn btn-danger btn-sm">delete</button>
+                        </div>
                     </div>
-
+                    <br>
+                    
+                    <button id="deleteIceCream" type="button" class="btn btn-danger btn-sm">Delete Ice Cream</button>
+                    
                 </div>
-
-                <div class="row align-items-center">
-                    <div class="col-sm">
-                        <p>new toppings here</p>
-
-                    </div>
-                    <div class="col-sm">
-                        <button id="deleteTopping" type="button" class="btn btn-danger btn-sm">delete</button>
-                    </div>
-                </div>
-                <br>
-                
-                <button id="deleteIceCream" type="button" class="btn btn-danger btn-sm">Delete Ice Cream</button>
-                
             </div>
-        </div>
-            `;
-        });
-        iceCreamList.innerHTML = output;
-    })
+                `;
+            });
+            iceCreamList.innerHTML = output;
+        })
+
+}
+
 
 
